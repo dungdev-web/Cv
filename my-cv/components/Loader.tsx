bbc3fce3c853019012cd76ea135f501cc91d8dd6
LƯU ĐỨC DŨNG <dung.dev.web@gmail.com>
2026-01-28 11:38:09 +0700

    update loader

--- my-cv/components/Loader.tsx
+++ my-cv/components/Loader.tsx
@@ -1,28 +1,39 @@
 export default function Loader() {
   return (
+    // <div className="loader-overlay">
+    //   <div className="loader">
+    //     <div className="ph1">
+    //       <div className="record"></div>
+    //       <div className="record-text">REC</div>
+    //     </div>
+    //     <div className="ph2">
+    //       <div className="laptop-b"></div>
+    //       <svg
+    //         className="laptop-t"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         viewBox="0 0 42 30"
+    //       >
+    //         <path
+    //           d="M21 1H5C2.78 1 1 2.78 1 5V25a4 4 90 004 4H37a4 4 90 004-4V5c0-2.22-1.8-4-4-4H21"
+    //           pathLength="100"
+    //           strokeWidth="2"
+    //           stroke="currentColor"
+    //           fill="none"
+    //         ></path>
+    //       </svg>
+    //     </div>
+    //     <div className="icon"></div>
+    //   </div>
+    // </div>
     <div className="loader-overlay">
       <div className="loader">
-        <div className="ph1">
-          <div className="record"></div>
-          <div className="record-text">REC</div>
-        </div>
-        <div className="ph2">
-          <div className="laptop-b"></div>
-          <svg
-            className="laptop-t"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 42 30"
-          >
-            <path
-              d="M21 1H5C2.78 1 1 2.78 1 5V25a4 4 90 004 4H37a4 4 90 004-4V5c0-2.22-1.8-4-4-4H21"
-              pathLength="100"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-            ></path>
-          </svg>
-        </div>
-        <div className="icon"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
       </div>
     </div>
   );
